c1d9899d1b6861be02cc85e4593fe636316fa97c
Baptiste Candellier <baptiste@candellier.me>
2025-12-07 23:05:30 +0100

    remove message permission

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -2,15 +2,10 @@ package fr.outadoc.bruitage.app
 
 import dev.kord.core.Kord
 import dev.kord.core.behavior.interaction.response.respond
-import dev.kord.core.entity.User
 import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEvent
-import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
-import dev.kord.gateway.Intent
-import dev.kord.gateway.PrivilegedIntent
 import dev.kord.rest.builder.interaction.string
 
-@OptIn(PrivilegedIntent::class)
 suspend fun main() {
     val token = checkNotNull(System.getenv("BOT_TOKEN"))
     val clientId = checkNotNull(System.getenv("BOT_CLIENT_ID"))
@@ -44,30 +39,9 @@ suspend fun main() {
         }
     }
 
-    kord.on<MessageCreateEvent> {
-        // runs every time a message is created that our bot can read
-
-        val author: User =
-            message.author ?: return@on
-
-        // ignore other bots, even ourselves. We only serve humans here!
-        if (author.isBot) return@on
-
-        when (message.content) {
-            "ping" -> {
-                message.channel.createMessage("pong!")
-            }
-
-            else -> {}
-        }
-    }
-
     println("Bot is ready and listening")
 
-    kord.login {
-        // we need to specify this to receive the content of messages
-        intents += Intent.MessageContent
-    }
+    kord.login()
 }
 
 fun createAuthUrl(clientId: String): String {
