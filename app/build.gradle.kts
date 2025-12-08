af2c10b20f28fb3062a4703ddbe252b182276f12
Baptiste Candellier <baptiste.candellier@dejamobile.com>
2025-12-08 13:32:20 +0100

    chore: clean up deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,19 @@
 plugins {
-    // Apply the shared build logic from a convention plugin.
-    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
-    id("buildsrc.convention.kotlin-jvm")
-
-    // Apply the Application plugin to add support for building an executable JVM application.
+    alias(libs.plugins.kotlin.jvm)
     application
 }
 
+kotlin {
+    jvmToolchain(21)
+}
+
 dependencies {
-    implementation("dev.kord:kord-core:0.17.0")
-    implementation("dev.kord:kord-core-voice:0.17.0")
-    implementation("dev.kord:kord-voice:0.17.0")
+    implementation(libs.kord.core)
+    implementation(libs.kord.core.voice)
+    implementation(libs.kord.voice)
+    implementation(libs.kotlinx.io.core)
 }
 
 application {
-    // Define the Fully Qualified Name for the application main class
-    // (Note that Kotlin compiles `App.kt` to a class with FQN `com.example.app.AppKt`.)
     mainClass = "fr.outadoc.bruitage.app.AppKt"
 }
